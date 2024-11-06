099fd84488050f87d9c52b25acf01e248cd31102
難波隼人 <hayatonanba@gmail.com>
2024-11-06 21:11:18 +0900

    fix: 不必要なpタグの削除,４０４の位置の修正

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -9,7 +9,7 @@ export default function NotFound() {
 				<div className="flex justify-center p-5">
 					<Image src="/404hukidashi.png" width={400} height={200} alt="" />
 					<div className="absolute top-[50%] left-[50%] w-full translate-x-[-50%] translate-y-[-50%] text-center">
-						<h1 className="mx-auto mb-[250px] font-bold text-8xl">404</h1>
+						<h1 className="mx-auto mb-[280px] font-bold text-8xl">404</h1>
 					</div>
 				</div>
 				<div className="text-center font-bold">
@@ -17,10 +17,8 @@ export default function NotFound() {
 					<p className="p-6 text-2xl">ご迷惑をおかけして申し訳ございません。</p>
 				</div>
 				<div className="flex justify-center pt-10">
-					<Button className="h-[50px] w-[300px]">
-						<Link href="/">
-							<p className="font-bold text-xl">トップへ戻る</p>
-						</Link>
+					<Button className="h-[50px] w-[300px] font-bold text-xl">
+						<Link href="/">トップへ戻る</Link>
 					</Button>
 				</div>
 			</div>
