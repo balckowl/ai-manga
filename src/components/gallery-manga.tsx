eb08d3f4d4848ee3214fe7ec0f4cbe4adcb9cc70
Soma Sekine <a8121151@aoyama.jp>
2024-11-08 01:07:22 +0900

    feat: ギャラリーページ見た目だけ

--- src/components/gallery-manga.tsx
+++ src/components/gallery-manga.tsx
@@ -1,5 +1,17 @@
+'use client';
+
 import Manga from './manga'
 import { FaRegTrashAlt } from "react-icons/fa";
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
+
+import { Button } from "@/components/ui/button"
 
 type Coma = {
 	text: string;
@@ -17,7 +29,41 @@ export default function GalleryManga({comaList}: Props) {
             <Manga title="こんにちは" comaList={comaList} />
             <div className='flex justify-between'>
                 <time dateTime='2022-11-07'>2022/11/07</time>
-                <FaRegTrashAlt className="transition-colors duration-200 hover:text-red-400" />
+                <Dialog>
+                    <DialogTrigger>
+                        <FaRegTrashAlt className="transition-colors duration-200 hover:text-red-400" />
+                    </DialogTrigger>
+
+                    <DialogContent className="max-w-[400px]">
+                        <DialogHeader>
+                        <DialogTitle className="mx-auto mb-10 text-center">作品の削除</DialogTitle>
+                        <DialogDescription className='text-center text-black text-md'>
+                                作品を削除するよ
+                                <br />
+                                元に戻せないから注意してね！
+                            <FaRegTrashAlt className="mx-auto my-[65px] text-9xl text-black" />
+                        </DialogDescription>
+
+                        <div className="grid grid-cols-1 gap-2 md:grid-cols-2">
+                            <Button
+                                type="button"
+                                variant="default"
+                                onClick={() => console.log("削除するボタンが押されました")}
+                                className="font-bold"
+                                >削除する
+                            </Button>
+                            <Button
+                                type="button"
+                                variant="outline"
+                                onClick={() => console.log("キャンセルボタンが押されました")}
+                                className="border-2 border-black border-solid font-bold"
+                                >キャンセル
+                            </Button>
+                        </div>
+                        
+                        </DialogHeader>
+                    </DialogContent>
+                </Dialog>
             </div>
         </article>
     )
