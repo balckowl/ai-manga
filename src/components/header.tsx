2a60c6653de00a905705b5f88ece0b4eac0582a6
難波隼人 <hayatonanba@gmail.com>
2024-11-07 18:33:48 +0900

    feat: ヘッダーのログインを実装

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,22 +1,82 @@
-import { signIn } from "@/auth";
+import { auth, signIn, signOut } from "@/auth";
 import Image from "next/image";
+import { PiSignOut } from "react-icons/pi";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Button } from "./ui/button";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 
-export default function Header() {
+export default async function Header() {
+	const session = await auth();
+	console.log(session);
 	return (
 		<header className="h-[100px]">
 			<div className="container mx-auto flex h-full items-center justify-between">
 				<Image src="/logo.png" alt="" width={209} height={33} />
-				<form
-					action={async () => {
-						"use server";
-						await signIn("google");
-					}}
-				>
-					<Button variant="ghost" className="text-[20px]">
-						ログイン
-					</Button>
-				</form>
+				{!session && (
+					<form
+						action={async () => {
+							"use server";
+							await signIn("google");
+						}}
+					>
+						<Button variant="ghost" className="text-[20px]">
+							ログイン
+						</Button>
+					</form>
+				)}
+				{session && (
+					<div className="flex items-center gap-7">
+						<ul className="flex gap-5">
+							<li>ホーム</li>
+							<li>つくる</li>
+							<li>お気に入り</li>
+							<li>自分の作品</li>
+							<li>みんなの作品</li>
+						</ul>
+						<Popover>
+							<PopoverTrigger asChild>
+								<Avatar>
+									<AvatarImage
+										src={session.user?.image as string}
+										alt={session.user?.name as string}
+									/>
+									<AvatarFallback>{session.user?.name}</AvatarFallback>
+								</Avatar>
+							</PopoverTrigger>
+							<PopoverContent className="w-[250px] p-0" align="end">
+								<ul className="space-y-5 p-8">
+									<li className="flex items-center gap-3">
+										<Avatar>
+											<AvatarImage
+												src={session.user?.image as string}
+												alt={session.user?.name as string}
+											/>
+											<AvatarFallback>{session.user?.name}</AvatarFallback>
+										</Avatar>
+										<p>{session.user?.name}</p>
+									</li>
+									<li>
+										<form
+											action={async () => {
+												"use server";
+												await signOut();
+											}}
+										>
+											<Button
+												variant="ghost"
+												className="flex w-full items-center justify-start gap-3"
+												type="button"
+											>
+												<PiSignOut />
+												ログアウト
+											</Button>
+										</form>
+									</li>
+								</ul>
+							</PopoverContent>
+						</Popover>
+					</div>
+				)}
 			</div>
 		</header>
 	);
--- src/components/top-manga.tsx
+++ src/components/top-manga.tsx
@@ -1,20 +1,19 @@
-import Manga from './manga'
+import Manga from "./manga";
 
 type Coma = {
 	text: string;
 	imageUrl: string;
 };
 
-
 type Props = {
-    comaList: Coma[];
-}
+	comaList: Coma[];
+};
 
-export default function TopManga({comaList}: Props) {
-    return (
-        <article>
-            <Manga title="こんにちは" comaList={comaList} />
-            <p className="text-3 text-[#808080]">宮沢賢治先生yo・2022/11/07</p>
-        </article>
-    )
+export default function TopManga({ comaList }: Props) {
+	return (
+		<article>
+			<Manga title="こんにちは" comaList={comaList} />
+			<p className="text-3 text-[#808080]">宮沢賢治先生yo・2022/11/07</p>
+		</article>
+	);
 }
