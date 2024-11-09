2144a575cc16587822a6c29a7782fdb32c0b32cf
難波隼人 <hayatonanba@gmail.com>
2024-11-09 13:17:31 +0900

    feat: 作品が一個もない場合のUIを追加

--- src/app/(pages)/gallery/page.tsx
+++ src/app/(pages)/gallery/page.tsx
@@ -1,7 +1,10 @@
 import { auth } from "@/auth";
 import GalleryManga from "@/components/gallery-manga";
 import SubTitle from "@/components/sub-title";
+import { Button } from "@/components/ui/button";
 import { getAllMyComics } from "@/data/comic";
+import Image from "next/image";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default async function Page() {
@@ -11,12 +14,31 @@ export default async function Page() {
 	return (
 		<div className="container mx-auto pt-[50px]">
 			<SubTitle title="自分が作った作品" />
-			<div className="mx-auto my-7 grid w-[95%] grid-cols-1 gap-9 md:grid-cols-3">
-				{allMyComics.map((myComic, i) => (
-					// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-					<GalleryManga key={i} myComic={myComic} />
-				))}
-			</div>
+			{allMyComics.length === 0 && (
+				<div className="space-y-10 text-center">
+					<div className="flex justify-center">
+						<Image src="/emptylocket.png" alt="" width={150} height={150} />
+					</div>
+					<div className="space-y-3">
+						<h1 className="font-bold text-3xl">まだ、作品がないみたい</h1>
+						<p>
+							作品を作ったらここに表示されるよ <br /> [あたらしくつくる]から作品を作ろう！
+						</p>
+					</div>
+					<Button className="h-[40px] w-[200px] font-bold">
+						<Link href="/new">あたらしくつくる</Link>
+					</Button>
+				</div>
+			)}
+			,
+			{allMyComics.length > 0 && (
+				<div className="mx-auto my-7 grid w-[95%] grid-cols-1 gap-9 md:grid-cols-3">
+					{allMyComics.map((myComic, i) => (
+						// biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+						<GalleryManga key={i} myComic={myComic} />
+					))}
+				</div>
+			)}
 		</div>
 	);
 }
