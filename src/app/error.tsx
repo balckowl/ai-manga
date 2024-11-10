6376294a62f1ad22dd4a5e4dc31da05911982f9a
難波隼人 <hayatonanba@gmail.com>
2024-11-10 16:47:30 +0900

    feat: エラーページの作成

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,5 +1,28 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
 export default function ErrorPage({ error }: { error: Error }) {
-	return <div>{error.name}</div>;
+	return (
+		<div className="flex h-[calc(100vh-200px)] items-center justify-center text-center">
+			<div className="space-y-7 font-bold">
+				<div className="flex justify-center">
+					<Image src="/error.png" alt="" width={150} height={150} />
+				</div>
+				<h1 className="text-3xl">エラーが発生しました</h1>
+				<div className="space-y-4">
+					<p>
+						申し訳ございません <br /> リクエストを正しく処理 <br />
+						できませんでした。
+					</p>
+					<p>(エラーコード: {error.name})</p>
+				</div>
+				<Button className="mx-auto h-[40px] w-[200px] font-bold">
+					<Link href="/">トップへ戻る</Link>
+				</Button>
+			</div>
+		</div>
+	);
 }
