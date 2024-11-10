5f953d8c8d5e139dc5bbf3ac80d14e08c746682d
難波隼人 <hayatonanba@gmail.com>
2024-11-10 20:16:07 +0900

    feat: いいねのuiを追加

--- src/components/top-manga.tsx
+++ src/components/top-manga.tsx
@@ -1,5 +1,6 @@
 import type { SelectComic, SelectUser } from "@/db/schema";
 import { format } from "date-fns";
+import { CiHeart } from "react-icons/ci";
 import Manga from "./manga";
 
 type Props = {
@@ -13,9 +14,15 @@ export default function TopManga({ comicWithAuthor }: Props) {
 	return (
 		<article>
 			<Manga title={title} contents={contents} />
-			<p className="text-3 text-[#808080]">
-				{user?.name}先生・{format(publishedAt, "yyyy/MM/dd")}
-			</p>
+			<div className="flex justify-between">
+				<p className="text-3 text-[#808080]">
+					{user?.name}先生・{format(publishedAt, "yyyy/MM/dd")}
+				</p>
+				<div className="flex items-center gap-3">
+					<p className="text-3 text-[#808080]">xxxx</p>
+					<CiHeart />
+				</div>
+			</div>
 		</article>
 	);
 }
